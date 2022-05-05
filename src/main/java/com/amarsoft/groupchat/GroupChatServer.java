011fc726c1ec3279bf24fb55a25326710b0687d7
yhk <1492860368@qq.com>
2022-05-05 11:20:30 +0800

    nacos群聊系统

--- src/main/java/com/amarsoft/groupchat/GroupChatServer.java
+++ src/main/java/com/amarsoft/groupchat/GroupChatServer.java
@@ -28,7 +28,7 @@ public class GroupChatServer {
                     .childOption(ChannelOption.SO_KEEPALIVE,true)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        protected void initChannel(SocketChannel ch) throws Exception {
+                        protected void initChannel(SocketChannel ch) {
                             ChannelPipeline pipeline = ch.pipeline();
                             pipeline.addLast("decoder",new StringDecoder());
                             pipeline.addLast("encoder",new StringEncoder());
