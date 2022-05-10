22fb9f6cda01c5f38b0840ea3ca34edad30f2457
yhk <1492860368@qq.com>
2022-05-10 14:15:27 +0800

    netty

--- src/main/java/com/amarsoft/Dubbo/netty/NettyClient.java
+++ src/main/java/com/amarsoft/Dubbo/netty/NettyClient.java
@@ -1,9 +1,7 @@
 package com.amarsoft.Dubbo.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.ChannelPipeline;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -16,46 +14,42 @@ import java.util.concurrent.Executors;
 
 public class NettyClient {
 
-    //创建线程池
     private static ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     private static NettyClientHandler client;
 
-    public Object getBean(final Class<?> serivceClass, final String providerName) {
-
+    public Object getBean(final Class<?> serviceClass,final String providername){
         return Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
-                new Class<?>[]{serivceClass}, (proxy, method, args) -> {
-                    if (client == null) {
+                new Class<?>[] {serviceClass},(proxy, method, args) -> {
+                    if (client == null){
                         initClient();
                     }
-                    client.setPara(providerName + args[0]);
+                    client.setPara(providername+args[0]);
                     return executor.submit(client).get();
                 });
     }
 
-    private static void initClient() {
+    private static void initClient(){
         client = new NettyClientHandler();
-        NioEventLoopGroup group = new NioEventLoopGroup();
-        Bootstrap bootstrap = new Bootstrap();
-        bootstrap.group(group)
-                .channel(NioSocketChannel.class)
-                .option(ChannelOption.TCP_NODELAY, true)
-                .handler(
-                        new ChannelInitializer<SocketChannel>() {
-                            @Override
-                            protected void initChannel(SocketChannel ch) {
-                                ChannelPipeline pipeline = ch.pipeline();
-                                pipeline.addLast(new StringDecoder());
-                                pipeline.addLast(new StringEncoder());
-                                pipeline.addLast(client);
-                            }
-                        }
-                );
+        EventLoopGroup group = new NioEventLoopGroup();
         try {
-            bootstrap.connect("127.0.0.1", 4000).sync();
-        } catch (Exception e) {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new StringDecoder());
+                            pipeline.addLast(new StringEncoder());
+                            pipeline.addLast(client);
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 4000).sync();
+            System.out.println("客户端开始提供服务");
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
 }
-
